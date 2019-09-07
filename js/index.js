34a0a5f064b358363b5bf4dd05501bdf2eb7c0be
rich92105 <52473445+rich92105@users.noreply.github.com>
2019-09-06 20:52:32 -0700

    almost done, need preventDefault to work

--- js/index.js
+++ js/index.js
@@ -19,5 +19,45 @@ aTags.forEach((nav) => {
   nav.addEventListener('mouseover', (event) => {
     nav.style.transform = 'scale(1.4)';
     nav.style.transition = 'transform 0.5s'; 
+ 
+  })
+ 
+});
+
+
+aTags.forEach((nav) => {
+  nav.addEventListener('mouseout', (event) => {
+    nav.style.transform = 'scale(1.0)';
+    nav.style.transition = 'transform 0.5s'; 
+  })
+});
+
+
+
+const imagesAll = document.querySelectorAll ('img');
+imagesAll.forEach((img) => {
+  img.addEventListener('dblclick', (event) => {
+img.src = 'https://cdn.admonsters.com/wp-content/uploads/2018/07/doubleclick-660.png';
+img.alt ='double click me'
+  })
+});
+
+
+const funBus = document.querySelector ('h1');
+funBus.addEventListener('auxclick', (event) => {
+alert ('you should not copy this');
+
+})
+
+
+
+
+
+
+//* prevent default not working
+
+aTags.forEach((nav) => {
+  nav.addEventListener(click, (event) => {
+    nav.preventDefault();
   })
 });
