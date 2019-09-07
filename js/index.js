0d07246f199f98b777626cec0c7b1d20d0bad2c7
rich92105 <52473445+rich92105@users.noreply.github.com>
2019-09-06 19:58:09 -0700

    2 events so far

--- js/index.js
+++ js/index.js
@@ -1,1 +1,23 @@
 // Your code goes here
+
+
+
+
+
+
+const allButtons = document.querySelectorAll ('.btn');
+
+allButtons.forEach((button) => (
+  button.addEventListener('click', (event) => {      
+    button.style.transform = 'scale(1.3)';
+     button.style.transition = 'transform 0.5s'; 
+  })
+));
+
+const aTags = document.querySelectorAll ('a');
+aTags.forEach((nav) => {
+  nav.addEventListener('mouseover', (event) => {
+    nav.style.transform = 'scale(1.4)';
+    nav.style.transition = 'transform 0.5s'; 
+  })
+});
